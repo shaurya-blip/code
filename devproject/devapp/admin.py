d6cf5f2381d40f02daea7265e7de898942774775
Shaurya Pratap Singh <65819451+shaurya-blip@users.noreply.github.com>
2020-08-20 10:14:58 +0530

    Update admin.py

--- devproject/devapp/admin.py
+++ devproject/devapp/admin.py
@@ -1,8 +1,3 @@
-# from github repository shaurya-blip/code
-# link -> https://www.github.com/shaurya-blip/code
-# Copyrights registered shaurya-blip2020
-
-
 from django.contrib import admin
 from .models import Post,Comment
 # Register your models here.
